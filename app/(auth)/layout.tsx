b906b755e5201fe251e5956e77c7531412fa5d7c
hakanyorukk <hakanyorukomer@hotmail.com>
2024-09-16 13:04:20 +0300

    Added demo message to login page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex min-h-screen w-full font-inter">
+    <main className="relative flex min-h-screen w-full font-inter">
+      <div className="fixed top-0  w-full left-0 right-0  bg-amber-600 flex justify-center items-center ">
+        <p className="text-slate-950 text-sm sm:text-xl px-6 py-2">
+          👋 An example login credentials has been pre-filled. Simply click Sign
+          In to proceed.
+        </p>
+      </div>
       <div className="basis-1/2 bg-gradient-to-tr from-teal-700 to-indigo-950 hidden xl:block">
         <div className="mt-16">
           <Logo />
